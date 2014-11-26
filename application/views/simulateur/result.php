01faba18f8f6768aecca4590014d875d6af3170c
anouerH <anouermail@gmail.com>
2014-11-26 13:48:49 +0100

    update

---
+++ application/views/simulateur/result.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <h1>Calcul des consommations de chauffage</h1>
+    <code>CchPCI = CchPCS / apcsi</code><br />
+    <var>y</var> = <var>m</var><var>x</var> + <var>b</var><br />
+    <samp>This text is meant to be treated as sample output from a computer program.</samp>
+    <h3>Calcul de Bch</h3>
+</div>
